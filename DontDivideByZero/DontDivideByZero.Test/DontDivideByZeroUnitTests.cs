ddbeec3a182a15c9269f13acf35ab6f2b42b5901
Alexey <alexeyzakis@gmail.com>
2023-03-01 23:30:03 +0300

    Explicit zero division and tests

---
+++ DontDivideByZero/DontDivideByZero.Test/DontDivideByZeroUnitTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using VerifyCS = DontDivideByZero.Test.CSharpCodeFixVerifier<
+    DontDivideByZero.DontDivideByZeroAnalyzer,
+    DontDivideByZero.DontDivideByZeroCodeFixProvider>;
+
+namespace DontDivideByZero.Test {
+    [TestClass]
+    public class DontDivideByZeroUnitTest {
+       [TestMethod]
+        public async Task NoZero_noDiagnostic() {
+            var test = @"
+class MyProgram {
+    static void Main() {
+        int a = {|#0:1 / 1|};
+    }
+}
+";
+            await VerifyCS.VerifyAnalyzerAsync(test);
+       }
+        
+        [TestMethod]
+        public async Task ExplicitDivisionOfAConstantByZero_Diagnostic() {
+            var test = @"
+class MyProgram {
+    static void Main() {
+        int a = {|#0:1 / 0|};
+    }
+}
+";
+            var expDiag = new DiagnosticResult(DontDivideByZeroAnalyzer.DiagnosticId, DiagnosticSeverity.Error);
+            await VerifyCS.VerifyAnalyzerAsync(test, expDiag.WithLocation(0), DiagnosticResult.CompilerError("CS0020").WithLocation(0));
+        }
+        
+        [TestMethod]
+        public async Task ExplicitDivisionOfAFunctionByZero_Diagnostic() {
+            var test = @"
+class MyProgram {
+    static int f() {
+        return 1;
+    }
+
+    static void Main() {
+        int a = {|#0:f() / 0|};
+    }
+}
+";
+            var expDiag = new DiagnosticResult(DontDivideByZeroAnalyzer.DiagnosticId, DiagnosticSeverity.Error);
+            await VerifyCS.VerifyAnalyzerAsync(test, expDiag.WithLocation(0));
+        }
+        
+        [TestMethod]
+        public async Task SimpleArithmeticSubtraction_Diagnostic() {
+            var test = @"
+class MyProgram {
+    static void Main() {
+        int a = 1;
+        int b = {|#0:1 / (a - a)|};
+    }
+}
+";
+            var expDiag = new DiagnosticResult(DontDivideByZeroAnalyzer.DiagnosticId, DiagnosticSeverity.Error);
+            await VerifyCS.VerifyAnalyzerAsync(test, expDiag.WithLocation(0), DiagnosticResult.CompilerError("CS0020").WithLocation(0));
+        }
+        
+        [TestMethod]
+        public async Task SimpleArithmeticMultiplication_Diagnostic() {
+            var test = @"
+class MyProgram {
+    static void Main() {
+        int a = 1;
+        int b = {|#0:1 / (0 * a)|};
+    }
+}
+";
+            var expDiag = new DiagnosticResult(DontDivideByZeroAnalyzer.DiagnosticId, DiagnosticSeverity.Error);
+            await VerifyCS.VerifyAnalyzerAsync(test, expDiag.WithLocation(0), DiagnosticResult.CompilerError("CS0020").WithLocation(0));
+        }
+        
+        [TestMethod]
+        public async Task DivisionByVariable_Diagnostic() {
+            var test = @"
+class MyProgram {
+    static void Main() {
+        int a = 0;
+        int b = {|#0:1 / a|};
+    }
+}
+";
+            var expDiag = new DiagnosticResult(DontDivideByZeroAnalyzer.DiagnosticId, DiagnosticSeverity.Error);
+            await VerifyCS.VerifyAnalyzerAsync(test, expDiag.WithLocation(0), DiagnosticResult.CompilerError("CS0020").WithLocation(0));
+        }
+    }
+}
---
+++ DontDivideByZero/DontDivideByZero/DontDivideByZeroAnalyzer.cs
@@ -0,0 +1,63 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+ using Microsoft.CodeAnalysis.Text;
+
+ namespace DontDivideByZero {
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DontDivideByZeroAnalyzer : DiagnosticAnalyzer {
+        public const string DiagnosticId = "DontDivideByZero";
+        
+        private static readonly LocalizableString Title = new LocalizableResourceString(
+            nameof(Resources.AnalyzerTitle), 
+            Resources.ResourceManager, 
+            typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(
+            nameof(Resources.AnalyzerMessageFormat), 
+            Resources.ResourceManager, 
+            typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(
+            nameof(Resources.AnalyzerDescription),
+            Resources.ResourceManager, 
+            typeof(Resources));
+        
+        private const string Category = "Error";
+        
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId, 
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Error,
+            isEnabledByDefault: true,
+            description: Description
+            );
+        
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics {
+            get {
+                return ImmutableArray.Create(Rule);
+            }
+        }
+
+        public override void Initialize(AnalysisContext context) {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.DivideExpression);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context) {
+            var binExp = (BinaryExpressionSyntax)context.Node;
+            
+            if (!context.SemanticModel.GetConstantValue(binExp.Right).Value.Equals(0)) {
+                return;
+            }
+            context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation()));
+        }
+    }
+}
